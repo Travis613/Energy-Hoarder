27aa636ab460419b3c1e129971340083a0d98282
Travis <travism.github@gmail.com>
2026-09-14 13:13:46 -0400

    cleaned up the projects folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Sign_up from "./pages/Sign-up";
-import Help from "./pages/Help";
+import Home from "./components/custom/pages/Home";
+import Login from "./components/custom/pages/Login";
+import Sign_up from "./components/custom/pages/Sign-up";
+import Help from "./components/custom/pages/Help";
 
 function App() {
   return (
